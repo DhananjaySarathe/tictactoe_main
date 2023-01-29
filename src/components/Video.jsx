a8b4d9ebe83d2569ce1273e61664e94e7894f331
DhananjaySarathe <68105420+DhananjaySarathe@users.noreply.github.com>
2023-01-29 17:48:01 +0530

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Video(){
     return <div class="alert alert-secondary bg-success video" role="alert">
-    <video height="300px" controls muted autoplay><source src="video/Tictactoe.mp4" alt="video"  type="video/mp4" /></video>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/4WNbTj3nfOA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
 }
 
