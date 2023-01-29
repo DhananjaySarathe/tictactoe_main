b2a0038ee38debbe67b4fe5ec59527397e420d8c
DhananjaySarathe <68105420+DhananjaySarathe@users.noreply.github.com>
2023-01-29 17:41:38 +0530

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Video(){
     return <div class="alert alert-secondary bg-success video" role="alert">
-    <video height="300px" controls muted autoplay><source src="video/TicTacToe.mp4" alt="video"  type="video/mp4" /></video>
+    <video height="300px" controls muted autoplay><source src="video/Tictactoe.mp4" alt="video"  type="video/mp4" /></video>
   </div>
 }
 
